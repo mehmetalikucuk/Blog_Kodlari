a683214c36844fb3a16e217d5fbef273d9dbacc2
mehmetalikucuk <36423878+mehmetalikucuk@users.noreply.github.com>
2019-01-28 22:30:19 +0300

    Add files via upload

---
+++ linear_regression.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("direnc.csv")
+gerilim = data.Gerilim.values.reshape(-1,1)
+akim = data.Akim.values.reshape(-1,1)
+
+plt.scatter(gerilim,akim,label="Örnekler")
+plt.xlabel("Gerilim[V]")
+plt.ylabel("Akım[A]")
+plt.legend()
+
+#%%
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression(fit_intercept=False)
+model.fit(gerilim,akim)
+
+tahminler = model.predict(gerilim)
+
+plt.plot(gerilim,tahminler,"red",label="Linear Model")
+plt.legend()
+
+print("2.5V gerilimindeki akım değeri: ",model.predict(2.5))
+print("Denklem: ","V=",model.intercept_,"+",model.coef_,"*I")
---
+++ polinomial_regression.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("diyot.csv")
+gerilim = data.Gerilim.values.reshape(-1,1)
+akim = data.Akim.values.reshape(-1,1)
+
+plt.scatter(gerilim,akim,label="Örnekler")
+plt.xlabel("Gerilim[V]")
+plt.ylabel("Akım[mA]")
+plt.legend()
+
+#%%
+from sklearn.linear_model import LinearRegression
+model = LinearRegression(fit_intercept=False)
+model.fit(gerilim,akim)
+tahminler = model.predict(gerilim)
+
+plt.plot(gerilim,tahminler,"red",label="Linear Model")
+plt.legend()
+
+#%%
+from sklearn.preprocessing import PolynomialFeatures
+model2 = PolynomialFeatures(degree=4)
+gerilim_poly = model2.fit_transform(gerilim) 
+
+model3 = LinearRegression(fit_intercept=False)
+model3.fit(gerilim_poly,akim)
+tahminler2 = model3.predict(gerilim_poly)
+
+plt.plot(gerilim,tahminler2,"black",label="Polynomial Model")
+plt.legend()
