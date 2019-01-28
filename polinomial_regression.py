37bfc1680d6329f5e7b3a0a0f7020ff9f2ae4992
mehmetalikucuk <36423878+mehmetalikucuk@users.noreply.github.com>
2019-01-28 22:35:48 +0300

    Update polinomial_regression.py

--- polinomial_regression.py
+++ polinomial_regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+# Veri okuma ve gorsellestirme
 data = pd.read_csv("diyot.csv")
 gerilim = data.Gerilim.values.reshape(-1,1)
 akim = data.Akim.values.reshape(-1,1)
@@ -9,8 +9,8 @@ plt.scatter(gerilim,akim,label="Örnekler")
 plt.xlabel("Gerilim[V]")
 plt.ylabel("Akım[mA]")
 plt.legend()
-
-#%%
+# ------------------------------
+# Linear model olusturma ve gorsellestirme
 from sklearn.linear_model import LinearRegression
 model = LinearRegression(fit_intercept=False)
 model.fit(gerilim,akim)
@@ -18,8 +18,8 @@ tahminler = model.predict(gerilim)
 
 plt.plot(gerilim,tahminler,"red",label="Linear Model")
 plt.legend()
-
-#%%
+# ------------------------------
+# Polynomial model olusturma ve gorsellestirme
 from sklearn.preprocessing import PolynomialFeatures
 model2 = PolynomialFeatures(degree=4)
 gerilim_poly = model2.fit_transform(gerilim) 
@@ -30,3 +30,4 @@ tahminler2 = model3.predict(gerilim_poly)
 
 plt.plot(gerilim,tahminler2,"black",label="Polynomial Model")
 plt.legend()
+# ------------------------------
