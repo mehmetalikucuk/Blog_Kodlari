168132422cd90ceff71d396532233f2e30ef4a91
mehmetalikucuk <36423878+mehmetalikucuk@users.noreply.github.com>
2019-01-28 22:33:37 +0300

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+# Veri okuma ve gorsellestirme
 data = pd.read_csv("direnc.csv")
 gerilim = data.Gerilim.values.reshape(-1,1)
 akim = data.Akim.values.reshape(-1,1)
@@ -9,8 +9,8 @@ plt.scatter(gerilim,akim,label="Örnekler")
 plt.xlabel("Gerilim[V]")
 plt.ylabel("Akım[A]")
 plt.legend()
-
-#%%
+# ------------------------------
+# Model olusturma ve gorsellestirme
 from sklearn.linear_model import LinearRegression
 
 model = LinearRegression(fit_intercept=False)
@@ -20,6 +20,8 @@ tahminler = model.predict(gerilim)
 
 plt.plot(gerilim,tahminler,"red",label="Linear Model")
 plt.legend()
-
+# ------------------------------
+# Model denklemini ogrenme
 print("2.5V gerilimindeki akım değeri: ",model.predict(2.5))
 print("Denklem: ","V=",model.intercept_,"+",model.coef_,"*I")
+# ------------------------------
